d6b32769a23478e906fea2f9c63cb2d7181d6674
Jibon <manvirjibon@gmail.com>
2024-01-25 20:39:36 +0600

    log req.body with post method

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express =require("express");
 const app =express();
 const port =7000;
+const signUpSchema =require('./models/signUpSchema')
+app.use(express.json())
 app.get('/', (req,res)=>{
     res.send("sfsfsf")
+})
+app.post('/' , (req,res)=>{
+    const {FirstName,LastName,EmailAdress,MobileNumber,PresentAdress,City,PostCode,Country,Region,Password}=req.body
+    console.log(req.body);
+
 })
 app.listen(port,()=>{
     console.log("the local host port is running :7000");
