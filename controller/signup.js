eee4550a6ecfb0eb7aacaf2cf5d16dcc5195c5a1
Jibon <manvirjibon@gmail.com>
2024-01-26 20:09:25 +0600

    signup data write mongodb

--- controller/signup.js
+++ controller/signup.js
@@ -1,21 +1,21 @@
-// const signUpSchema = require("../models/signUpSchema");
+const signUpSchema = require("../models/signUpSchema");
 
-//  function signup (req,res){
-//     const {FirstName,LastName,EmailAdress,MobileNumber,PresentAdress,City,PostCode,Country,Region,Password}=req.body
-//         console.log(req.body);
-//         const signUpData=new signUpSchema({
-//             FirstName,
-//             LastName,
-//             EmailAdress,
-//             MobileNumber,
-//             PresentAdress,
-//             City,
-//             PostCode,
-//             Country,
-//             Region,
-//             Password
-//         })
-//         signUpData.save();
-//         res.send(signUpData)
-//  }
-//  module.exports =signup
+ function signup (req,res){
+    const {FirstName,LastName,EmailAdress,MobileNumber,PresentAdress,City,PostCode,Country,Region,Password}=req.body
+        console.log(req.body);
+        const signUpData=new signUpSchema({
+            FirstName,
+            LastName,
+            EmailAdress,
+            MobileNumber,
+            PresentAdress,
+            City,
+            PostCode,
+            Country,
+            Region,
+            Password
+        })
+        signUpData.save();
+        res.send(signUpData)
+ }
+ module.exports =signup
