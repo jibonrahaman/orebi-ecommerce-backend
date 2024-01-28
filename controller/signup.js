cfdbaeb36e7a98edcd83b63afff5cbc8c3b1c702
Jibon <manvirjibon@gmail.com>
2024-01-28 23:14:06 +0600

    bycript hash password done

--- controller/signup.js
+++ controller/signup.js
@@ -1,23 +1,27 @@
 const signUpSchema = require("../models/signUpSchema");
 const SendEmailVerify = require("../nodemailer/SendEmailVerify");
-
+const bcrypt = require('bcrypt');
  function signup (req,res){
     const {FirstName,LastName,EmailAdress,MobileNumber,PresentAdress,City,PostCode,Country,Region,Password}=req.body
-        console.log(req.body);
-        const signUpData=new signUpSchema({
-            FirstName,
-            LastName,
-            EmailAdress,
-            MobileNumber,
-            PresentAdress,
-            City,
-            PostCode,
-            Country,
-            Region,
-            Password
-        })
-        SendEmailVerify(EmailAdress)
-        signUpData.save();
-        res.send(signUpData)
+       
+        bcrypt.hash(Password, 10, function(err, hash) {
+            const signUpData=new signUpSchema({
+                FirstName,
+                LastName,
+                EmailAdress,
+                MobileNumber,
+                PresentAdress,
+                City,
+                PostCode,
+                Country,
+                Region,
+                Password:hash
+            })
+            SendEmailVerify(EmailAdress)
+            signUpData.save();
+            res.send(signUpData)
+        });
+      
+      
  }
  module.exports =signup
