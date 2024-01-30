97b27c26be411cff0bef355f4f39d30a01d53134
Jibon <manvirjibon@gmail.com>
2024-01-30 19:31:25 +0600

    verify email notification html minifier

--- nodemailer/SendEmailVerify.js
+++ nodemailer/SendEmailVerify.js
@@ -13,9 +13,8 @@ const nodemailer = require("nodemailer");
       const info = await transporter.sendMail({
         from: 'OREBI', // sender address
         to: email, // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
+        subject: "Email Verification", 
+        html: `<div><img alt=""src=https://i.ibb.co/qdqTcby/OREBI.png style=margin-left:50px;margin-top:30px><p style=font-weight:700;font-size:20px;color:teal>Please Verify Your Email</p><button style=padding:10px;border-radius:10px;background-color:#000;color:#fff;font-weight:700;font-size:15px;margin-left:35px>Confim Verify</button></div>`, // html body
       });
  }
   module.exports =SendEmailVerify
