73a9a50345576f660adbda06565d41f10e39ec26
Jibon <manvirjibon@gmail.com>
2024-01-30 19:11:43 +0600

    emailVerify route setup

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -1,5 +1,7 @@
 const express =require("express");
 const signup = require("../../controller/signup");
+const emailVeryfied = require("../../controller/emailVeryfied");
  const authentication =express();
  authentication.post('/signup',signup )
+ authentication.post( '/emailVerify', emailVeryfied)
   module.exports =authentication;
