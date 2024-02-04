798fffd3963d8f3dcdaa64f291d33baae9870b87
Jibon <manvirjibon@gmail.com>
2024-02-04 15:03:09 +0600

    CategoryShcema created

--- models/CategorySchema.js
+++ models/CategorySchema.js
@@ -2,6 +2,26 @@ const mongoose =require ("mongoose");
 const {Schema} = mongoose;
 
 const CategorySchema = new Schema ({
-
+ namae:{
+    type:String,
+    required:true
+ },
+ description:String,
+ isActive:{
+    type:Boolean,
+    default:false
+ },
+ status:{
+    type:String,
+    default:"waiting",
+    enum:['waiting','rejected','approved']
+ },
+ created:{
+    type:Date,
+    default:new Date()
+ },
+ update:{
+    type:Date
+ }
 })
 module.exports =mongoose.model("CategoryList", CategorySchema)
