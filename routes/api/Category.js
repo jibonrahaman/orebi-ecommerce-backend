ba71cf8672e2a265d584f9b5f08a4d4a1116021c
Jibon <manvirjibon@gmail.com>
2024-02-04 15:24:55 +0600

    api Category

---
+++ routes/api/Category.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const Category = express();
+
+module.exports = Category;
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,4 @@ const router=express.Router();
 const apiRouter =require('./api')
 const baseApi=process.env.BASE_URL
 router.use(baseApi, apiRouter)
-module.exports =router;
+module.exports =router; 
