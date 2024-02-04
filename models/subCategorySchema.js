b2c16c0f31725df7a726c5c189f56b79a0d06443
Jibon <manvirjibon@gmail.com>
2024-02-04 15:09:58 +0600

    subCategorySchema

---
+++ models/subCategorySchema.js
@@ -0,0 +1,27 @@
+const mongoose = require ("mongoose");
+const {Schema} =mongoose;
+
+const subCategorySchema =new Schema({
+    namae:{
+        type:String,
+        required:true
+     },
+     description:String,
+     isActive:{
+        type:Boolean,
+        default:false
+     },
+     status:{
+        type:String,
+        default:"waiting",
+        enum:['waiting','rejected','approved']
+     },
+     created:{
+        type:Date,
+        default:new Date()
+     },
+     update:{
+        type:Date
+     }
+})
+module.exports = mongoose.model("SubCategoryList", subCategorySchema)
