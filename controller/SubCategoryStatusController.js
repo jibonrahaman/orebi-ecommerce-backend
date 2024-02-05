62ce4e6f099a47bf814c4d07513e818558aebd80
Jibon <manvirjibon@gmail.com>
2024-02-05 11:06:48 +0600

    sub Category status update

--- controller/SubCategoryStatusController.js
+++ controller/SubCategoryStatusController.js
@@ -1,4 +1,15 @@
-function SubCategoryStatus (req,res){
+const subCategorySchema = require("../models/subCategorySchema");
 
+async function SubCategoryStatus (req,res){
+  const {name,status} =req.body;
+   if(status == 'waiting' || status == "rejected"){
+    const updateSubCategoryStatus = await subCategorySchema.findOneAndUpdate(
+        {name},
+        {$set:{isActive:false,status:status}},
+        {new:true}
+    )
+   }else if (status == "approved"){
+    const updateCategoryStatus = await subCategorySchema.findOneAndUpdate()
+   }
 } 
 module.exports = SubCategoryStatus;
