b799e56a4015f339d0186450e7a21e41d1f051c6
Jibon <manvirjibon@gmail.com>
2024-02-05 10:48:05 +0600

    SubCategory Schema send data mongodb & find this

--- controller/subCategoryController.js
+++ controller/subCategoryController.js
@@ -1,3 +1,24 @@
-function subCategoryController (req,res){
-  
+const subCategorySchema = require("../models/subCategorySchema");
+
+async function subCategoryController (req,res){
+   const {name,description,subCategory} =req.body;
+   try{
+     const existsubCategory =await subCategorySchema.find({name})
+    if(existsubCategory.length > 0){
+        res.json({error:"This SubCategory is already exist"});
+     }
+   
+     const SubCategory = new subCategorySchema({
+        name,
+        description,
+        subCategory
+     })
+     res.json({success:"Sub Category Created Successfully"});
+     SubCategory.save();
+
+   } catch(error){
+    res.json({error:"internal server"});
+    console.log(error);
+   }
+
 } module.exports = subCategoryController;
