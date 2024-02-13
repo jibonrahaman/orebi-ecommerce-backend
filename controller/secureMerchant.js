ccd92f7537f11953f4b31848bb9a66b3c983be6e
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-13 22:47:00 +0600

    await data

--- controller/secureMerchant.js
+++ controller/secureMerchant.js
@@ -1,4 +1,20 @@
+const signUpSchema = require("../models/signUpSchema");
+
 function secureMerchant (req,res,next){
- const userId =  (req.headers.authorization.split('@')[1])
+ const userId =  (req.headers.authorization.split('@')[1]);
+ const userPass =  (req.headers.authorization.split('@')[2]);
+  
+  try{
+    if(!req.headers.authorization){
+        res.json({error: "Unauthorization"})
+    }else{
+        const user = signUpSchema.find({_id : userId})
+        console.log(user);
+    }
+
+  }catch(error){
+    console.log(error);
+  }
+
 }
 module.exports = secureMerchant;
